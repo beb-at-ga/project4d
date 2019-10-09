ebf3ef2a78de3b3a372ca6cf24b7a4353e5b7bfd
Bryan Brinson <bryan@beb.com>
2019-10-09 15:58:40 -0700

    start of express app

---
+++ backend/controllers/auth.js
@@ -0,0 +1,92 @@
+require("dotenv").config();
+const router = require("express").Router();
+const db = require("../models");
+const jwt = require("jsonwebtoken");
+
+// POST /auth/login (find and validate user and send password)
+router.post("/login", (req, res) => {
+  // Find the user by their email address
+  db.User.findOne({
+    email: req.body.email
+  })
+    .then(user => {
+      if (!user || !user.password) {
+        return res.status(406).send({
+          message: "Invalid Credentials"
+        });
+      }
+
+      if (!user.isAuthenticated(req.body.password)) {
+        // password did not match
+        return res.status(406).send({
+          message: "Invalid credentials"
+        });
+      }
+
+      let token = jwt.sign(user.toJSON(), process.env.JWT_SECRET, {
+        expiresIn: 60 * 60
+      });
+
+      res.send({ token });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(503).send({
+        message: "Something is amiss."
+      });
+    });
+});
+
+router.post("/signup", (req, res) => {
+  // res.send(req.body);
+  db.User.findOne({
+    email: req.body.email
+  })
+    .then(user => {
+      // if user exists, do not let them create a duplicate account
+      if (user) {
+        return res.status(409).send({
+          message: "Email address already in use."
+        });
+      } else {
+        db.User.create(req.body)
+          .then(newUser => {
+            // assing user a JWT
+            let token = jwt.sign(newUser.toJSON(), process.env.JWT_SECRET, {
+              expiresIn: 60 * 60 // expiration in seconds.
+            });
+
+            res.send({
+              token
+            });
+          })
+          .catch(err => {
+            console.log(err);
+            res.status(500).send({
+              message: "Error creating new customer record."
+            });
+          });
+      }
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(503).send({
+        message: "Error accessing database. Please try again."
+      });
+    });
+});
+
+router.get("/current/user", (req, res) => {
+  if (!req.user) {
+    res.status(500).send({ message: 'Something went wrong. Please try again.'})
+  } else {
+    // This is the user data from the time the token was issued, therfore if the user is updated,
+    // those values will not be reflected here...
+
+    // to avoid this problem, reissue token when you update their data. 
+    // login, update, 
+    res.send({user: req.user});
+  }
+});
+
+module.exports = router;
---
+++ backend/index.js
@@ -0,0 +1,52 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+// const db = require("./models");
+const expressJwt = require("express-jwt");
+
+const app = express();
+app.use(cors());
+
+const rowdyResults = require("rowdy-logger").begin(app);
+
+app.use(
+  express.urlencoded({
+    extended: false
+  })
+);
+
+app.use(morgan("dev"));
+app.use(
+  express.json({
+    limit: "30mb"
+  })
+);
+
+app.get("/", (req, res) => {
+  res.send("hello");
+});
+
+// app.use(
+//   "/auth",
+//   expressJwt({
+//     secret: process.env.JWT_SECRET
+//   }).unless({
+//     path: [
+//       { url: "/auth/login", methods: ["POST"] },
+//       { url: "/auth/signup", methods: ["POST"] }
+//     ]
+//   }),
+//   require("./controllers/auth")
+// );
+
+app.get("*", (req, res) => {
+  res.status(404).send({
+    message: "doh!"
+  });
+});
+
+app.listen(process.env.PORT || 3000, () => {
+  rowdyResults.print();
+  console.log("On Port: 3000");
+});
