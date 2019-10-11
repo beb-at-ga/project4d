1d9912a7d9e5158ffaa2a22f09379b88216396b2
Bryan Brinson <bryan@beb.com>
2019-10-11 12:02:07 -0700

    couple comment cleanups

--- emailHandler/handler.js
+++ emailHandler/handler.js
@@ -1,25 +1,17 @@
 'use strict';
 
-// define the options for your email and domain
 const options = {
-  myEmail: process.env.EMAIL, // myEmail is the email address you enabled in AWS SES in the AWS Console
-  myDomain: process.env.DOMAIN // add the domain of your website or '*' if you want to accept requests from any domain
+  myEmail: process.env.EMAIL, 
+  myDomain: process.env.DOMAIN  //for cors, domain name or '*' for any domain
 }
  
-// initialize the function
 const { sendJSON, sendFormEncoded } = require('./lambdaMailerCorreto')(options)
- 
-// Content-Type: application/json
-// The event.body needs to be a JSON object with 3 properties
-// - email
-// - name
-// - content
-// module.exports.sendJSON = sendJSON
  
 // Content-Type: application/x-www-form-urlencoded
 // The event.body needs to a URI encoded string with 3 parameters
 // - email
 // - name
 // - content
+// - subject
 module.exports.sendFormEncoded = sendFormEncoded
 
